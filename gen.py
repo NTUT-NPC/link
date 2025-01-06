3cc2d36ed0330f9ca8b0acd71c16379c22887cc5
kevinlee-06 <cgshyuankai@proton.me>
2025-01-06 17:51:37 +0800

    Refactor NPC files and update template with dynamic background colors

--- gen.py
+++ gen.py
@@ -1,6 +1,19 @@
-import csv, os, shutil
+import csv, os, shutil, random
 
 links_dir = 'docs'
+colors = ['red', 'green', 'blue', 'orange', 'purple', 'pink', 'brown', 'black']
+
+gradient_colors = {
+    'red': ['#5C2E2E', '#7A3B3B', '#9B4A4A', '#B05C5C', '#C76B6B'],
+    'green': ['#2E4D2E', '#3B6A3B', '#4A8B4A', '#5CA05C', '#6FB76F'],
+    'blue': ['#2E4B7A', '#3B6A9B', '#4A8BC4', '#5C9BC4', '#6FB3D1'],
+    'orange': ['#7A4D2E', '#A65B3B', '#C46A4A', '#D78B5C', '#E7A76B'],
+    'purple': ['#4B2E5C', '#6A3B7A', '#8B4A9B', '#A65CB0', '#B76BC4'],
+    'pink': ['#5C2E4B', '#7A3B6A', '#9B4A8B', '#B05CA6', '#C76BB7'],
+    'brown': ['#4E3B2E', '#6D4A3B', '#8B5C4A', '#A76B5C', '#C76B4A'],
+    'black': ['#000000'],
+}
+
 
 for item in os.listdir(links_dir):
     item_path = os.path.join(links_dir, item)
@@ -15,11 +28,14 @@ for filename in os.listdir(links_dir):
         os.makedirs(output_dir, exist_ok=True)
 
         links = ""
-
+        selected_color = random.choice(colors)
         with open(os.path.join(links_dir, filename), mode='r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                links += f'<a href="{row["URL"]}">{row["Title"]}</a>\n'
+                background_colors = gradient_colors[selected_color]
+                bg_color = random.choice(background_colors)
+                links += f'<a href="{row["URL"]}" style="background-color: {bg_color};">{row["Title"]}</a>\n'
+
 
         with open('template.html', 'r') as template_file:
             template = template_file.read()
